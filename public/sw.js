b12157ae6acea533d7f385dfbf7c7e262fb9766b
darshan2008-collab <darshankannan2008@gmail.com>
2026-08-04 10:08:21 +0530

    Fix PWABuilder manifest icon links, types, sizes, add service worker sw.js and app screenshots

---
+++ public/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'friends-mobile-v1';
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/favicon.svg',
+  '/icon-192.svg',
+  '/icon-512.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => caches.match('/'));
+    })
+  );
+});
